97339a22a81141dc26671465c69eb12571ab1779
bl4ckfl4me-dev <bl4ckfl4me-dev@yandex.ru>
2024-12-20 18:43:54 +0400

    add gost

--- hash.py
+++ hash.py
@@ -1,3 +1,15 @@
+matrix = (
+    (4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3),
+    (14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9),
+    (5, 8, 1, 13, 10, 3, 4, 2, 14, 15, 12, 7, 6, 0, 9, 11),
+    (7, 13, 10, 1, 0, 8, 9, 15, 14, 4, 6, 12, 11, 2, 5, 3),
+    (6, 12, 7, 1, 5, 15, 13, 8, 4, 10, 9, 14, 0, 3, 11, 2),
+    (4, 11, 10, 0, 7, 2, 1, 13, 3, 6, 8, 5, 9, 12, 15, 14),
+    (13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12),
+    (1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12),
+)
+
+
 def xor_bytes(a: bytes, b: bytes) -> bytes:
     return bytes(x ^ y for x, y in zip(a, b))
 
@@ -13,20 +25,43 @@ def key_schedule(key: bytes) -> list:
     return round_keys
 
 
+def get_out(inright: int, key: bytes):
+    out = 0
+    temp = (inright + int(key)) % (1 << 32)
+    for i in range(8):
+        phonetic = (temp >> (4 * i)) & 0b1111
+        out |= (matrix[i][phonetic] << (4 * i))
+    out = ((out >> 21) | (out << 11)) & 0xFFFFFFFF
+    return out
+
+
+def crypt_operation(inleft, inright, key):
+    outleft = inright
+    outright = inleft ^ get_out(inright, key)
+    return outleft, outright
+
+
+def gost_encrypt(text:bytes, key):
+    text = int(text.hex(), 16)
+    text_left = text >> 32
+    text_right = text & 0xFFFFFFFF
+    for q in range(24):
+        text_left, text_right = crypt_operation(text_left, text_right, key[q % 8])
+    for q in range(8):
+        text_left, text_right = crypt_operation(text_left, text_right, key[7 - q])
+    hash = (text_left << 32) | text_right
+    return hash
+
+
 def des_encrypt(block: bytes, key: bytes, m_i: bytes) -> bytes:
     if len(block) != 8 or len(key) != 8 or len(m_i) != 8:
         raise ValueError("Block, key, and m_i must all be 8 bytes.")
 
-    left = block[:4]
-    right = block[4:]
-    round_keys = key_schedule(key)
+    temp = xor_bytes(block[4:], m_i)
+    key_schedule_gost = key_schedule(key)
+    encrypted = gost_encrypt(temp, key_schedule_gost)
 
-    temp = xor_bytes(right, m_i)
-    for round_key in round_keys:
-        new_right = xor_bytes(temp, round_key)
-        left, temp = temp, xor_bytes(left, new_right)
-
-    return temp + left
+    return encrypted[:4] + block[:4]
 
 
 def pad_message(message: bytes, block_size: int) -> bytes:
